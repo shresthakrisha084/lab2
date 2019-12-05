5973c46472a0aeec066d5033086dc76a895810c7
shresthakrisha084 <shresthakrisha084@gmail.com>
2019-12-05 11:13:40 +0545

    ss

--- linked.cpp
+++ linked.cpp
@@ -16,6 +16,13 @@ void Linkedlist::addToHead(int data){
 	HEAD=newNode;
 }
 
+void Linkedlist::add(int data,Node* &predecessor){
+	Node* newNode=new Node();
+	newNode->info=data;
+	newNode->next=predecessor->next;
+	predecessor->next=newNode;
+}
+
 void Linkedlist::traverse(){
 	Node* temp=HEAD;
 	
@@ -25,11 +32,44 @@ void Linkedlist::traverse(){
 }
 std::cout<<std::endl;
 }
+void Linkedlist::removefromHead(){
+	Node* toDelete=HEAD;
+	HEAD= HEAD->next;
+	if (HEAD==NULL){
+		TAIL=NULL;
+	}
+	delete toDelete;
+	
+}
+
+bool Linkedlist::retrieve(int data,Node* &outputptr){
+	   	Node* temp=HEAD;
+		while(temp!=NULL && temp-> info!=data){		
+			temp=temp->next;
+	}
+	if (temp==NULL){
+		return false;
+	}
+	else{
+		outputptr=temp;
+		return true;
+	}
+	
+	}
 int main()
 {
+	
 	Linkedlist s;
 	s.addToHead(8);
 	s.addToHead(5);
 	s.traverse();
+	s.removefromHead();
+	s.traverse();
+	Node* p=new Node();
+	bool found=s.retrieve(8,p);
+	if (found)
+	{
+		s.add(4,p);
+	}
 	
 }
--- linked.h
+++ linked.h
@@ -15,6 +15,11 @@ class Linkedlist
 		~Linkedlist();
 		bool isEmpty();
 		void addToHead(int data);
+		void add(int data,Node* &predecessor);
+		void addToTail(int data);
+		void removefromHead();
+		void remove(int data);
 		void traverse();
+		bool retrieve(int data,Node* &outputptr);
 };
 
